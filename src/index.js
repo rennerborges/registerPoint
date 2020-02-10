5becbbe1a3048a28afd1795a8b456dbc345ff20c
Renner Borges <rennerferreira23@gmail.com>
2020-02-10 16:36:38 -0300

    Remoção de import inutil

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Teste from './components/teste';
 import PrivateRoute from './components/PrivateRoute';
 import Hooks from './components/Hooks';
 
